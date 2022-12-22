1b511c20726fb747acec00c33054c5aefcdfb1a0
Osama Abdelaziz <osama.abdelaziz23@gmail.com>
2022-12-22 03:01:47 +0200

    add locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:posts_clean_bloc_app/core/app_theme.dart';
 import 'package:posts_clean_bloc_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
 import 'package:posts_clean_bloc_app/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:posts_clean_bloc_app/features/posts/presentation/pages/posts_page.dart';
 import 'injection_container.dart' as di;
 
+GetIt locator = GetIt.instance;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  locator.registerLazySingleton<PostsBloc>(() => PostsBloc(getAllPosts: locator())) ;
   await di.init();
   runApp(MyApp());
 }
